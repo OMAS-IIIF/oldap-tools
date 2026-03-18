502ccd498041a4a8e95131e450fda0ce7bd3f57b
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2026-03-19 00:45:48 +0100

    Adding dump parameters for admin and datamodel graphs

--- src/oldap_tools/dump_project.py
+++ src/oldap_tools/dump_project.py
@@ -25,6 +25,8 @@ def dump_project(project_id: str,
                  repo: str,
                  out: Path,
                  include_data: bool,
+                 include_model: bool,
+                 include_admin: bool,
                  user: str,
                  password: str,
                  graphdb_user: str | None = None,
@@ -50,37 +52,40 @@ def dump_project(project_id: str,
     trig += f"# Project: {project.projectShortName}\n"
     trig += f"# Date: {datetime.now().isoformat()}\n"
     trig += "################################################################################\n"
-    trig += "\n#\n# User info\n#\n"
-    userIris = User.search(con=con, inProject=project.projectIri)
-    for userIri in userIris:
-        user = User.read(con=con, userId=str(userIri))
-        user_json = json.dumps(user, default=serializer.encoder_default)
-        trig += "#>> " + user_json + "\n"
-    trig += "#<<\n\n"
 
     context = Context(name=con.context_name)
-    trig += context.turtle_context
-    trig += "#\n# Load the oldap:admin part\n#\n"
-    trig += "oldap:admin {"
+    if include_admin:
+        trig += "\n#\n# User info\n#\n"
+        userIris = User.search(con=con, inProject=project.projectIri)
+        for userIri in userIris:
+            user = User.read(con=con, userId=str(userIri))
+            user_json = json.dumps(user, default=serializer.encoder_default)
+            trig += "#>> " + user_json + "\n"
+        trig += "#<<\n\n"
 
-    trig += "\n#\n# Project info\n#\n"
-    trig += project.trig_to_str(created=project.created, modified=project.modified, indent=1)
-    trig += " .\n\n"
+        trig += context.turtle_context
+        trig += "#\n# Load the oldap:admin part\n#\n"
+        trig += "oldap:admin {"
 
-    trig += "\n#\n# Roles info\n#\n"
-    roleQNames = Role.search(con=con, definedByProject=project.projectIri)
-    for roleQName in roleQNames:
-        role = Role.read(con=con, qname=roleQName)
-        trig += role.trig_to_str(created=role.created, modified=role.modified, indent=1)
+        trig += "\n#\n# Project info\n#\n"
+        trig += project.trig_to_str(created=project.created, modified=project.modified, indent=1)
         trig += " .\n\n"
 
-    trig += "\n}\n\n"
+        trig += "\n#\n# Roles info\n#\n"
+        roleQNames = Role.search(con=con, definedByProject=project.projectIri)
+        for roleQName in roleQNames:
+            role = Role.read(con=con, qname=roleQName)
+            trig += role.trig_to_str(created=role.created, modified=role.modified, indent=1)
+            trig += " .\n\n"
 
-    project_graphs = [
-        str(context.qname2iri(Xsd_QName(project.projectShortName, "shacl"))),
-        str(context.qname2iri(Xsd_QName(project.projectShortName, "onto"))),
-        str(context.qname2iri(Xsd_QName(project.projectShortName, "lists"))),
-    ]
+        trig += "\n}\n\n"
+
+    project_graphs = []
+    if include_model:
+        project_graphs.append(str(context.qname2iri(Xsd_QName(project.projectShortName, "shacl")))),
+        project_graphs.append(str(context.qname2iri(Xsd_QName(project.projectShortName, "onto")))),
+        project_graphs.append(str(context.qname2iri(Xsd_QName(project.projectShortName, "lists")))),
+
     if include_data:
         project_graphs.append(str(context.qname2iri(Xsd_QName(project.projectShortName, "data"))))
 
